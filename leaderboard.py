d09ff5de5842910577f759d55a58b0320240d99d
Isaac <ifrisch2@gmail.com>
2025-07-14 10:14:04 -0500

    Update leaderboard with fixed logo and styling

--- leaderboard.py
+++ leaderboard.py
@@ -22,7 +22,7 @@ st.markdown("""
     /* Logo block: no extra spacing */
     #logo-block {
         margin-top: 0rem;
-        margin-bottom: -3rem;
+        margin-bottom: -15rem;
         text-align: center;
     }
 
