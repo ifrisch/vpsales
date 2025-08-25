a1d906a8c6bf601fe849e6d86563c632267adc19
Isaac <ifrisch2@gmail.com>
2025-08-25 07:52:50 -0500

    Auto-update from Van Paper 07:29 AM on 2025-08-25

---
+++ final_automation.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Users\Isaac\OneDrive - Van Paper Company\Python_Projects\Sales_Leaderboard"
+"C:\Users\Isaac\AppData\Local\Programs\Python\Python313\python.exe" silent_automation.py
