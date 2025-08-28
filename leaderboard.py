058db3ab41e57414052512668328bcec00c3efdf
Isaac <ifrisch2@gmail.com>
2025-08-28 10:02:13 -0500

    One-click update from Van Paper 10:01 AM on 2025-08-28

--- leaderboard.py
+++ leaderboard.py
@@ -354,7 +354,7 @@ st.markdown('</div>', unsafe_allow_html=True)
 
 # --- TIMESTAMP ---
 central = ZoneInfo("America/Chicago")
-LAST_SYNC_TIMESTAMP = "2025-08-28 07:29:56"  # AUTO-UPDATED BY BATCH FILE
+LAST_SYNC_TIMESTAMP = "2025-08-28 10:02:13"  # AUTO-UPDATED BY BATCH FILE
 
 # Display sync timestamp
 sync_time = datetime.strptime(LAST_SYNC_TIMESTAMP, '%Y-%m-%d %H:%M:%S')
