222f78c30c47affb65aa409465bea9d5a0e7e4c3
Isaac <ifrisch2@gmail.com>
2025-10-24 07:26:54 -0500

    One-click update from Van Paper 07:06 AM on 2025-10-24

--- leaderboard.py
+++ leaderboard.py
@@ -464,7 +464,7 @@ if st.session_state.show_winner_popup:
 
 # --- TIMESTAMP ---
 central = ZoneInfo("America/Chicago")
-LAST_SYNC_TIMESTAMP = "2025-10-23 08:03:42"  # AUTO-UPDATED BY BATCH FILE
+LAST_SYNC_TIMESTAMP = "2025-10-24 07:26:54"  # AUTO-UPDATED BY BATCH FILE
 
 # Display sync timestamp
 sync_time = datetime.strptime(LAST_SYNC_TIMESTAMP, '%Y-%m-%d %H:%M:%S')
